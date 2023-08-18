3d173d737d4b2440d63f2fd7caab7a020975fabd
mraniki <8766259+mraniki@users.noreply.github.com>
2023-08-18 12:17:21 +0200

    Update main.py

--- myllm/main.py
+++ myllm/main.py
@@ -87,7 +87,7 @@ class MyLLM:
         self.logger.info(f"Starting chat with prompt: {prompt}")
         return g4f.ChatCompletion.create(
             # model=settings.llm_model,
-            model=importlib.import_module(settings.llm_model),
+            model=settings.llm_model,
             # provider=importlib.import_module(settings.llm_provider),
             messages=[{"role": "user", "content": prompt}],
         )

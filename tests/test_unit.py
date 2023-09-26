d8fcba6817ba724358b1477e0142275f774affd9
mraniki <mraniki@users.noreply.github.com>
2023-09-26 21:15:10 +0200

    :white_check_mark:

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -41,10 +41,11 @@ async def test_clear_chat_history(talky):
 
 @pytest.mark.asyncio
 async def test_switch_continous_mode(talky):
+    assert settings.llm_ai_mode is True
     result = await talky.switch_continous_mode()
     assert result is not None
     assert "Continous" in result
-    assert settings.llm_ai_mode is True
+    assert settings.llm_ai_mode is False
 
 
 @pytest.mark.asyncio

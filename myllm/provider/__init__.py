67419e167ea56802cc7d2339afc78e2b28eab78d
mraniki <8766259+mraniki@users.noreply.github.com>
2024-01-26 14:52:55 +0100

    🚨

--- myllm/provider/__init__.py
+++ myllm/provider/__init__.py
@@ -1,6 +1,5 @@
-
 from myllm.provider.g4f import MyLLMG4F
-from myllm.provider.openai import MyLLMOpenAI
 from myllm.provider.google_bard import MyLLMBard
+from myllm.provider.openai import MyLLMOpenAI
 
-__all__ = ["MyLLMG4F", "MyLLMOpenAI", "MyLLMBard""]
+__all__ = ["MyLLMG4F", "MyLLMOpenAI", "MyLLMBard"]

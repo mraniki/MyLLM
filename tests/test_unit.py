cedb571e294cb171d1cd6b540c78fd3cf6047664
mraniki <8766259+mraniki@users.noreply.github.com>
2024-01-26 16:13:45 +0100

    ✅ Unit Test

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -24,6 +24,24 @@ def test_fixture_myllm():
     return MyLLM()
 
 
+@pytest.fixture(name="llm_client")
+def client_fixture(talky):
+    for llm in talky.clients:
+        if llm.provider == "g4f":
+            return llm
+
+@pytest.fixture(name="bard_client")
+def bardclient_fixture(talky):
+    for llm in talky.clients:
+        if llm.provider == "bard":
+            return llm
+
+@pytest.fixture(name="openai_client")
+def openaiclient_fixture(talky):
+    for llm in talky.clients:
+        if llm.provider == "openai":
+            return llm
+
 @pytest.mark.asyncio
 async def test_get_myllm_info(talky):
     result = await talky.get_info()
@@ -32,23 +50,41 @@ async def test_get_myllm_info(talky):
 
 
 @pytest.mark.asyncio
-async def test_get_chats(talky):
-    result = await talky.get_chats("tell me a story")
-    assert result is not None
-
-@pytest.mark.asyncio
 async def test_myllmclient(talky):
     """Init Testing"""
     assert isinstance(talky, MyLLM)
     assert talky.clients is not None
     assert callable(talky.get_info)
     assert callable(talky.get_chats)
-    for client in talky.clients:
-        assert client is not None
-        assert client.llm_library is not None
-        assert client.llm_model is not None
-        assert client.llm_provider is not None
-        assert client.Conversation is not None
-        assert callable(client.export_chat_history)
-        assert callable(client.clear_chat_history)
-        assert callable(client.clear_chat_history)
+    for llm in talky.clients:
+        assert llm is not None
+        assert llm.llm_library is not None
+        assert llm.llm_model is not None
+        assert llm.llm_provider is not None
+        assert llm.Conversation is not None
+        assert callable(llm.export_chat_history)
+        assert callable(llm.clear_chat_history)
+        assert callable(llm.clear_chat_history)
+
+
+@pytest.mark.asyncio
+async def test_get_chats(talky):
+    result = await talky.get_chats("tell me a story")
+    assert result is not None
+
+
+@pytest.mark.asyncio
+async def test_chat_g4f(llm_client):
+    result = await llm_client.chat("tell me a story")
+    assert result is not None
+
+@pytest.mark.asyncio
+async def test_chat_openai(openai_client):
+    result = await openai_client.chat("tell me a story")
+    assert result is not None
+
+
+@pytest.mark.asyncio
+async def test_chat_bard(bard_client):
+    result = await bard_client.chat("tell me a story")
+    assert result is not None

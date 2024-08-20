54faff7375a38ce024a5db6268f0ddbd5fa71965
mraniki <8766259+mraniki@users.noreply.github.com>
2024-08-20 09:52:06 +0200

    update main.py

--- myllm/main.py
+++ myllm/main.py
@@ -270,6 +270,30 @@ class MyLLM:
             url = self.browser_url
         async with async_playwright() as playwright:
             browser = await playwright.chromium.launch(headless=self.browser_headless)
+            context = await browser.new_context(
+                locale="en-US",
+                timezone_id="America/Los_Angeles",
+                device_scale_factor=1,
+                screen={"width": 1920, "height": 1080},
+                extra_http_headers={
+                    "Accept": (
+                        "text/html,application/xhtml+xml,"
+                        "application/xml;q=0.9,*/*;q=0.8"),
+                    "Accept-Encoding": "gzip, deflate, br",
+                    "Accept-Language": "en-GB,en;q=0.9",
+                    "Cache-Control": "max-age=0",
+                    "Connection": "keep-alive",
+                    "Sec-Fetch-Dest": "document",
+                    "Sec-Fetch-Mode": "navigate",
+                    "Sec-Fetch-Site": "same-origin",
+                    "User-Agent": (
+                        "Mozilla/5.0 "
+                        "(Macintosh; Intel Mac OS X 10_15_7) "
+                        "AppleWebKit/605.1.15 (KHTML, like Gecko) "
+                        "Version/17.2.1 Safari/605.1.15"
+                    ),
+                },
+            )
             page = await browser.new_page()
             await page.goto(url)
             await asyncio.sleep(2)

c6a6fa4b3ce6859ed8a20d16497f9bff3ddc52a2
mraniki <8766259+mraniki@users.noreply.github.com>
2024-10-27 13:59:04 +0100

    🔥

--- myllm/handler/__init__.py
+++ myllm/handler/__init__.py
@@ -1,6 +1,6 @@
+# from myllm.handler.crawl4ai import Crawl4aiHandler
 from myllm.handler.g4f import G4fHandler
 from myllm.handler.groq import GroqHandler
 from myllm.handler.openai import OpenaiHandler
-from myllm.handler.crawl4ai import Crawl4aiHandler
 
-__all__ = ["Crawl4aiHandler","G4fHandler", "GroqHandler", "OpenaiHandler"]
+__all__ = ["G4fHandler", "GroqHandler", "OpenaiHandler"]
